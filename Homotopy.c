8f091a02592b89bd4434aa01fd92f8fdae290975
Peter Reheis <preheis@trincoll.edu>
2015-07-16 16:39:50 -0400

    Resolved memory leaks and memory overwrites.

--- Homotopy.c
+++ Homotopy.c
@@ -16,9 +16,9 @@ int Example3(double *A){
 	int i,j;
 	for(i = 0; i<N; i++){
 		A[i*N+i]= (double) (i+1);
-		for (j = 0; j<N; j++){
-			A[(j*N)+(j-1)]=1.0;
-			A[((j-1)*N)+j]=1.0;
+		for (j = 0; j<N-1; j++){
+			A[(j*N+j)+1] = 1;
+			A[(j*N+j)+N] = 1;
 		}	
 	}
 	return 0;
@@ -54,85 +54,149 @@ int COUNT(double *DD, double *OO, double PE){
 	d = 1.0;
 	for (i = 0; i<N; i++){
 		d = DD[i]-PE-((OO[i]*OO[i])/d);
-		printf("%f\n",d);
 		if (d<0.0){Count++;}
 	}
 	return Count;
 }
 
 /*This function reduces the step size, alters the homotopy parameter, and resets the eigenvector*/
-int ReduceStep(double *A, double *D, double *DD, double *XT, double *DDD, double *OO, double H, double PT, double Z, double Z1, double APP, int NS){
-	double *X = (double*)calloc(N,sizeof(double));
+int ReduceStep(double *A, double *D, double *DD, double *X, double *XT, double *DDD, double *OO, double H, double PT, double Z, double Z1, double APP, int NS){
 	double T;
 	H = H/2;
 	T = PT+H;
-	cblas_dcopy(N,XT,1,X,1);
+	cblas_dcopy(N,XT,1,X,1);// X <- XT
 	if (NS==0){
 		double PE = Z+Z1*H;
 		mainblock(A, D, DD, DDD, OO, X, XT, Z, Z1, H, T, PT, PE, NS);
 	}
 	else{
 		printf("Recomputing the predicted eigenvalue...\n");
-		Predict(A, D, DD, X, DDD, OO, H, T, Z, Z1, APP, NS);
+		Predict(A, D, DD, XT, X, DDD, OO, H, T, Z, Z1, APP, NS);
 	}		
 	return 0;	
 }
 
+int initialPrediction(int k){
+	    //Matrices
+		double *A = (double*)calloc(N*N,sizeof(double));
+		double *D = (double*)calloc(N*N,sizeof(double));
+		double *AD = (double*)calloc(N*N,sizeof(double));
+		//Vectors	
+		double *DD = (double*)calloc(N,sizeof(double));
+		double *DDD = (double*)calloc(N,sizeof(double));
+		double *OO = (double*)calloc(N,sizeof(double));
+		double *X = (double*)calloc(N,sizeof(double));	
+		double *XT = (double*)calloc(N,sizeof(double));
+		double *XTprime = (double*)calloc(N,sizeof(double));
+		//Other important variables
+		double alpha, beta, H, PT, T, Z, Z1, PE;	
+		int NS, i, j, incx, incy;
+		//clock_t t; 
+		Example3(A);
+		Initialize(A,D,DD,DDD,OO);
+		for (i = 0; i<N; i++){
+			for (j = 0; j<N; j++){
+				AD[i*N+j] = A[i*N+j]-D[i*N+j];		
+			}
+		}
+	
+		H = T = alpha = beta = 1.0;
+		PT = 0.0;
+		incx = incy = 1;	
+		NS = 0;
+
+		printf("Beginning Computation\n");
+
+		Z=DD[k-1];
+		XT[k-1]=1;
+		cblas_dcopy(N,XT,incx,X,incx);//X <- XT
+		//Compute first derivative
+		printf("Computing Z1\n");
+		cblas_dgemv(CblasRowMajor,CblasNoTrans,N,N,alpha,AD,N,XT,incx,beta,XTprime,incy); //XT' <- (A-D)*XT
+		Z1 = cblas_ddot(N,XT,incx,XTprime,incx);//Z1 <- XT'*((A-D)*XT)
+		//Predict eigenvalue with first order taylor method
+		PE=Z+Z1*H;
+		printf("Beginning Correction.\n");
+		free(AD);
+		free(XTprime);
+		mainblock(A, D, DD, DDD, OO, X, XT, Z, Z1, H, T, PT, PE, NS);
+	return 0;
+}
+
 /*Function recomputes PE using Hermite Interpolation.*/
 /*If T == 1, the eigenvalues are stored.*/
-int Predict(double *A, double *D, double *DD, double *Y, double *DDD, double *OO, double H, double T, double Z, double Z1, double APP, int NS){
-	double *XT = (double*)calloc(N,sizeof(double));
-	double *HA = (double*)calloc(N,sizeof(double));
-	double *EV = (double*)calloc(N,sizeof(double));
-	double *EVT= (double*)calloc(N*N,sizeof(double));	
+int Predict(double *A, double *D, double *DD, double *XT,double *X, double *DDD, double *OO, double H, double T, double Z, double Z1, double APP, int NS){
+	double *HA = (double*)calloc(N,sizeof(double));	
 	double OZ=Z, OZ1=Z1, Q, QQ, PH=H, PT=T,PE;
 	int i;
 
 	H = 1-PT; T = 1; NS+=1; 
 
-	cblas_dcopy(N,Y,1,XT,1); //XT<-Y
+	cblas_dcopy(N,X,1,XT,1); //XT<-X
 
-	if (T==1){EV[k-1]=Z;}
+	if (T==1){
+		printf("Storing the eigenvalue and eigenvector!\n");
+		printf("%.15f\n",APP);
+		///////////////////////
+		free(A);
+		free(D);
+		free(DD);
+		free(DDD);
+		free(OO);
+		free(X);
+		free(XT);
+		///////////////////////
+		k++;
+		printf("%d\n",k);
+		if (k>N){
+			exit(1);		
+		}	
+		initialPrediction(k);
+		return 0;	
+	}
 	else{
 		//Recompute Z1
-		HA[0]=DDD[0]*Y[0]+OO[1]*Y[1];
-		for (i = 1; i<N-1; i++){HA[i]=OO[i]*Y[i-1]+DDD[i]*Y[i]+OO[i+1]*Y[i+1];}
-		HA[N-1]=OO[N-1]*Y[N-2]+DDD[N-1]*Y[N-1];
+		HA[0]=DDD[0]*X[0]+OO[1]*X[1];
+		for (i = 1; i<N-1; i++){HA[i]=OO[i]*X[i-1]+DDD[i]*X[i]+OO[i+1]*X[i+1];}
+		HA[N-1]=OO[N-1]*X[N-2]+DDD[N-1]*X[N-1];
 		//Recompute PE
 		Q = pow(1+(PH/H),2);
 		QQ = Q*(H/PH);
 		PE=OZ+OZ1*(H+PH)+Q*((Z-OZ)-OZ1*PH)+QQ*(PH*(Z+OZ1)-2*(Z-OZ));
 
-		mainblock(A, D, DD, DDD, OO, Y, XT, Z, Z1, H, T, PT, PE, NS);
+		mainblock(A, D, DD, DDD, OO, X, XT, Z, Z1, H, T, PT, PE, NS);
 	}
+	free(HA);
 	return 0;
 }
 
 /*This function computes the residual of the vector obtain from II*/
-double computeRES(double* A, double* Y, double APP){
-	double *AY = (double*)calloc(N,sizeof(double));
-	double *APPY = (double*)calloc(N,sizeof(double));
-	float *RES = (float*)calloc(N,sizeof(float));	
+double computeRES(double* A, double* X, double APP){
+	double *AX = (double*)calloc(N,sizeof(double));
+	double *APPX = (double*)calloc(N,sizeof(double));
 	double maxRES,alpha,beta;
-	int incx,incy,i;
+	int incx,incy,i,j;
 	alpha = beta = 1.0;
 	incx = incy = 1;
-
-	cblas_dcopy(N,Y,incy,APPY,incy);//APPY <- Y
-	cblas_dscal(N,-APP,APPY,incy);//APPY <- APP*Y
-	cblas_dgemv(CblasRowMajor,CblasNoTrans,N,N,alpha,A,N,Y,incx,beta,AY,incy);//AY <- A*Y
-	for (i = 0; i<N; i++){RES[i] = AY[i]-APPY[i];}
+	cblas_dcopy(N,X,incx,APPX,incx);//APPY <- Y
+	cblas_dscal(N,-APP,APPX,incx);//APPY <- APP*Y
+	cblas_dgemv(CblasRowMajor,CblasNoTrans,N,N,alpha,A,N,X,incx,beta,AX,incx);//AY <- A*Y
+	cblas_daxpy(N,alpha,AX,incx,APPX,incx);
 	printf("Finding the maximal element...\n");
-	maxRES = cblas_isamax(N,RES,incx);//RES <- max(RES);
-	printf("The residual is %.5e\n",maxRES);
-	free(AY);
-	free(APPY);
-	free(RES);
+	maxRES = 0.0;
+	for (i = 0; i<N; i++){
+		if ((APPX[i])>maxRES){
+			maxRES = (APPX[i]);
+		}
+	}
+	printf("The residual is %.17e\n",maxRES);
+	free(AX);
+	free(APPX);
 	return maxRES;
 }
 
 /*Approximate an eigenvector with Inverse Iteration*/
-double * II(double *At, double *Y, double APP, int j){
+double * II(double *At, double *X, double APP, int j){
 	printf("Beginning Inverse Iteration...\n");
 	double *W = (double*)calloc(N*N,sizeof(double));
 	double *ID = (double*)calloc(N*N,sizeof(double));
@@ -147,24 +211,25 @@ double * II(double *At, double *Y, double APP, int j){
 		}	
 	}
 
-	norm = cblas_dnrm2(N,Y,1); //norm(X);
-	cblas_dscal(N,1/norm,Y,1); // X <- X*1/norm(X)
+	norm = cblas_dnrm2(N,X,1); //norm(X);
+	cblas_dscal(N,1/norm,X,1); // X <- X*1/norm(X)
     printf("Solving the system of linear equations...\n"); 
-    INFO = LAPACKE_dgesv(LAPACK_ROW_MAJOR,N,NRHS,W,LDA,IPIV,Y,LDB);			
-	norm = cblas_dnrm2(N,Y,1); //norm(X);
-	cblas_dscal(N,1/norm,Y,1); // X <- X*1/norm(X)
+    INFO = LAPACKE_dgesv(LAPACK_ROW_MAJOR,N,NRHS,W,LDA,IPIV,X,LDB);			
+	norm = cblas_dnrm2(N,X,1); //norm(X);
+	cblas_dscal(N,1/norm,X,1); // X <- X*1/norm(X)
 	
 	free(W);
 	free(ID);
 	free(IPIV);
 
 	printf("Ending Inverse Iteration\n");
-	printVec(Y);
-	return Y;
+	printf("The new approximate eigenvector is: \n");
+	printVec(X);
+	return X;
 }
 
 /*Approximate an eigenvalue with Rayleigh Quotient*/
-double RQI(double *At, double *Y, int j){
+double RQI(double *At, double *X, int j){
 	double *u = (double*)calloc(N,sizeof(double));
 	double norm,APP,SUM,alpha,beta;
 	int incx,incy,i;
@@ -173,13 +238,13 @@ double RQI(double *At, double *Y, int j){
 	incx = incy = 1;
 	printf("Beginning Rayleigh Quotient Iteration...\n");
 	//Peform Rayleigh Quotient Iteration
-	norm = cblas_dnrm2(N,Y,1); //norm(X)
-	printf("Norm = %.5f\n",norm);
-	cblas_dscal(N,1/norm,Y,incx); //X <- X*1/norm(X);
-	cblas_dgemv(CblasRowMajor,CblasNoTrans,N,N,alpha,At,N,Y,incx,beta,u,incy); //u <- At*X
-	APP = cblas_ddot(N,u,incx,Y,incx); //APP <- X'*u
+	norm = cblas_dnrm2(N,X,1); //norm(X)
+	cblas_dscal(N,1/norm,X,incx); //X <- X*1/norm(X);
+	cblas_dgemv(CblasRowMajor,CblasNoTrans,N,N,alpha,At,N,X,incx,beta,u,incy); //u <- At*X
+	APP = cblas_ddot(N,u,incx,X,incx); //APP <- X'*u
 	printf("Ending Rayleigh Quotient Iteration...\n");
-	printf("APP = %.5f\n",APP);
+	printf("The new approximate eigenvalue is...\n");
+	printf("APP = %.10e\n",APP);
 	free(u);
 	return APP;
 }
@@ -192,7 +257,7 @@ int mainblock(double *A, double *D, double *DD, double *DDD, double *OO, double 
 	
 	//Set the error tolerances for the program
 	eps = 1.11E-16;
-
+	EPS = EPS1 = EPS2 = EPS3 = NORM = SUMA = SUMB = RES = 0;
 	for (i = 0; i<N; i++){
 		SUMA+=abs(DD[i]);
 		SUMB+=abs(OO[i]);
@@ -209,7 +274,6 @@ int mainblock(double *A, double *D, double *DD, double *DDD, double *OO, double 
 	//////////////////////////////////////////////////
 
 	double *At = (double*)calloc(N*N,sizeof(double));
-	double *Y = (double*)calloc(N,sizeof(double));
 	
 	//Form At = D + T*(A-D);
 	for (i=0; i<N; i++){
@@ -218,35 +282,33 @@ int mainblock(double *A, double *D, double *DD, double *DDD, double *OO, double 
 		}
 	}
 
-	cblas_dcopy(N,X,1,Y,1);
 	APP = PE;
 	sc = COUNT(DD,OO,APP);
+	//printf("The number of sign changes is %d\n",sc);
 	
-	printf("The number of sign changes is %d\n",sc);
-	
-	if (sc!=k && sc!=k-1){ReduceStep(A,D,DD,XT,DDD,OO,H,PT,Z,Z1,APP,NS); return 0;}	
+	if (sc!=k && sc!=k-1){ReduceStep(A,D,DD,X,XT,DDD,OO,H,PT,Z,Z1,APP,NS); return 0;}	
 	if (sc==k){KK=1;}
 	if (sc==k-1){KK=0;}
 	
 	for (i = 1; i<=10; i++){
-		Y = II(At,Y,APP,1);
+		X = II(At,X,APP,1);
 		//Compute Residual
 		printf("Computing the residual...\n");
-		RES = computeRES(A,Y,APP);
+		RES = computeRES(A,X,APP);
 		printf("Epsilon is: %e\n",EPS1);
 
 		if (RES<=EPS1){
-
+			free(At);
 			sc = COUNT(DD,OO,APP-EPS1-EPS3);
 			printf("The number of sign changes is %d\n",sc);
 			if (sc==k-1){
 				printf("Recomputing the predicted eigenvalue...\n");
-				Predict(A,D,DD,Y,DDD,OO,H,T,Z,Z1,APP,NS);
+				Predict(A,D,DD,XT,X,DDD,OO,H,T,Z,Z1,APP,NS);
 				return 0;			
 			}
 			else if (sc>k-1){
 				printf("Reducing the step size...\n");
-				ReduceStep(A,D,DD,XT,DDD,OO,H,PT,Z,Z1,APP,NS);
+				ReduceStep(A,D,DD,X,XT,DDD,OO,H,PT,Z,Z1,APP,NS);
 				return 0;
 			}
 			else{
@@ -254,34 +316,31 @@ int mainblock(double *A, double *D, double *DD, double *DDD, double *OO, double 
 				printf("The number of sign changes is %d\n",sc);
 				if (sc>=k){
 					printf("Recomputing the predicted eigenvalue...\n");
-					Predict(A,D,DD,Y,DDD,OO,H,T,Z,Z1,APP,NS);
+					Predict(A,D,DD,XT,X,DDD,OO,H,T,Z,Z1,APP,NS);
 					return 0;				
 				}
 			}		
 		}
 		
-		APP = RQI(At,Y,1);
-		
+		APP = RQI(At,X,1);
 		if (KK==1 && APP>(PE+EPS2)){
 			printf("Reducing the step size...\n");
-			ReduceStep(A,D,DD,XT,DDD,OO,H,PT,Z,Z1,APP,NS);
+			ReduceStep(A,D,DD,X,XT,DDD,OO,H,PT,Z,Z1,APP,NS);
 			return 0;
 		}
 		
 		if (KK==0 && APP<(PE-EPS2)){
 			printf("Reducing the step size...\n");
-			ReduceStep(A,D,DD,XT,DDD,OO,H,PT,Z,Z1,APP,NS);
+			ReduceStep(A,D,DD,X,XT,DDD,OO,H,PT,Z,Z1,APP,NS);
 			return 0;
 		}
 		//Reduce step if too many iterations of II/RQI
 		if (i==10){
 			printf("Reducing the step size...\n");
-			ReduceStep(A,D,DD,XT,DDD,OO,H,PT,Z,Z1,APP,NS);
+			ReduceStep(A,D,DD,X,XT,DDD,OO,H,PT,Z,Z1,APP,NS);
 			return 0;
 		}
 	}
-	free(At);
-	free(Y);
 	return 0;		
 }
 
@@ -289,7 +348,7 @@ int mainblock(double *A, double *D, double *DD, double *DDD, double *OO, double 
 int printMat(double *A){
 	int i,j;
 	for (i=0; i<N; i++){
-			for (j=0; j<N; j++){printf("%.4f\t",A[i*N+j]);}
+			for (j=0; j<N; j++){printf("%f\t",A[i*N+j]);}
 			printf("\n");		
 	}
 	printf("\n");
@@ -297,69 +356,7 @@ int printMat(double *A){
 
 int printVec(double *X){
 	int i;
-	for (i = 0; i<N; i++){printf("%.4f\n",X[i]);}
+	for (i = 0; i<N; i++){printf("%f\n",X[i]);}
 	return 0;	
 }
 
-//Main
-int main(void){
-	//Other important variables
-	double alpha, beta, H, PT, T, Z, Z1, Z2, Z3, PE;	
-	int NS, i, j, incx, incy;
-	//clock_t t; 
-	//Matrices
-	double *A = (double*)calloc(N*N,sizeof(double));
-	double *D = (double*)calloc(N*N,sizeof(double));
-	double *AD = (double*)calloc(N*N,sizeof(double));
-	//Vectors	
-	//double *d = (double*)calloc(n,sizeof(double));
-	double *DD = (double*)calloc(N,sizeof(double));
-	double *DDD = (double*)calloc(N,sizeof(double));
-	double *OO = (double*)calloc(N,sizeof(double));
-	double *X = (double*)calloc(N,sizeof(double));	
-	double *XT = (double*)calloc(N,sizeof(double));
-	double *XTprime = (double*)calloc(N,sizeof(double));
-
-	Example3(A);
-	Initialize(A,D,DD,DDD,OO);
-	
-	for (i = 0; i<N; i++){
-		for (j = 0; j<N; j++){
-			AD[i*N+j] = A[i*N+j]-D[i*N+j];		
-		}
-	}
-	
-	H = T = alpha = beta = 1.0;
-	PT = 0.0;
-	incx = incy = 1;	
-	NS = 0;
-
-	//t = clock();
-	printf("Beginning Computation\n");
-	for (i = 0; i<N; i++){
-		Z=DD[i];
-		XT[i]=1;
-		cblas_dcopy(N,XT,incx,X,incx);//X <- XT
-		//Compute first derivative
-		printf("Computing Z1\n");
-		cblas_dgemv(CblasRowMajor,CblasNoTrans,N,N,alpha,AD,N,XT,incx,beta,XTprime,incy); //XT' <- (A-D)*XT
-		Z1 = cblas_ddot(N,XT,incx,XTprime,incx);//Z1 <- XT'*((A-D)*XT)
-		//Predict eigenvalue with first order taylor method
-		PE=Z+Z1*H;
-		printf("Beginning Correction.\n");
-		mainblock(A, D, DD, DDD, OO, X, XT, Z, Z1, H, T, PT, PE, NS);
-	}
-	//Free all allocated memory
-	free(A);
-	free(D);
-	free(AD);
-	free(DD);
-	free(DDD);
-	free(OO);
-	free(X);
-	free(XT);
-	free(XTprime);
-	//t = clock()-t;
-	//fprintf("Total execution time: %f seconds\n", (float)t/CLOCKS_PER_SEC);
-	return 0;
-}
